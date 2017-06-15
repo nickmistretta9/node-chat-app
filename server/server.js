a916a6277d12b7ecc525845ccc4c0d742722e58a
Nickmistretta9 <Nick@Nicks-MBP.home>
2017-06-14 20:21:29 -0400

    Added new message and create message events

--- server/server.js
+++ server/server.js
@@ -13,6 +13,17 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
 	console.log('New user connected');
+
+	socket.emit('newMessage', {
+		from: 'mike',
+		text: 'Hey, What is going on.',
+		createdAt: 123
+	});
+
+	socket.on('createMessage', (newMessage) => {
+		console.log('createMessage', newMessage);
+	});
+
 	socket.on('disconnect', () => {
 		console.log('User disconnected');
 	});
