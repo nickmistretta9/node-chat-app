8810d5132cf07659efbf2550812707c6c58cc10b
Nickmistretta9 <Nick@Nicks-MBP.home>
2017-06-14 20:31:06 -0400

    Emit new message on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,6 @@ var socket = io();
 socket.on('connect', function() {
 	console.log('Connected to server');
 
-	socket.emit('createMessage', {
-		to: 'jen',
-		text: 'Hey this is Nick'
-	});
 });
 socket.on('disconnect', function() {
 	console.log('Disconnected from server');
